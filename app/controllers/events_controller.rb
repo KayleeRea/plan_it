783da5c4c831dba8ee0f8f7432ba09b5895cfdf8
Kaylee Edmonson <kaylee.edmonson@gmail.com>
2014-05-02 10:10:47 -0600

    User can edit and update an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
   def new
     @event= Event.new
   end
 
   def create
-    @event= Event.create(event_name: params[:event_name], event_type: params[:event_type], date: params[:date], time: params[:time])
+    @event= Event.create(event_name: params[:event][:event_name], event_type: params[:event][:event_type], date: params[:event][:date], time: params[:event][:time])
     redirect_to events_path(@event)
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_name: params[:event][:event_name], event_type: params[:event][:event_type], date: params[:event][:date], time: params[:event][:time])
+    redirect_to events_path(@event)
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,23 +3,34 @@ require 'spec_helper'
 feature 'Plan It!' do
   scenario 'User can create new' do
     visit '/'
-    click_on 'Add Event'
-    click_on 'Start Planning!'
-    fill_in 'event_name', with: 'my party'
-    fill_in 'event_type', with: 'birthday for me'
-    fill_in 'date', with: '04/27/2014'
-    fill_in 'time', with: '7pm'
+    click_on 'Start Planning'
+    click_on 'Add Your Event'
+    fill_in 'event[event_name]', with: 'my party'
+    fill_in 'event[event_type]', with: 'birthday party'
+    fill_in 'event[date]', with: '04/27/2014'
+    fill_in 'event[time]', with: '7pm'
     click_on 'Add'
     expect(page).to have_content('my party')
+    expect(page).to have_content('birthday party')
+    expect(page).to have_content('04/27/2014')
+    expect(page).to have_content('7pm')
   end
 
-=begin
-  scenario 'User can update new' do
-    visit '/new/new'
-    click_on 'Edit'
-    fill_in 'event_name', with: 'my birthday party'
+  scenario 'User can edit an event' do
+    visit '/'
+    click_on 'Start Planning'
+    click_on 'Add Your Event'
+    fill_in 'event[event_name]', with: 'my party'
+    fill_in 'event[event_type]', with: 'birthday party'
+    fill_in 'event[date]', with: '04/27/2014'
+    fill_in 'event[time]', with: '7pm'
+    click_on 'Add'
+    click_on 'Edit Event'
+    fill_in 'event[event_name]', with: "Ellie's party"
+    fill_in 'event[event_type]', with: 'surprise birthday party'
+    fill_in 'event[date]', with: '04/27/2014'
+    fill_in 'event[time]', with: '7pm'
     click_on 'Update'
-    expect(page).to have_content('my birthday party')
+    expect(page).to have_content("Ellie's party")
   end
-=end
 end
